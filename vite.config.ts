2eb154748855a1ed2e8d2cd9e5763444f3050b2e
Abdulrahman <abd.elsayed@hotmail.com>
2026-03-16 23:46:07 +0200

    Installed Prettier and added related config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path";
+import { defineConfig } from 'vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,11 +11,11 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
-  resolve :{
+  resolve: {
     alias: {
-      '@':path.resolve(__dirname,'./src'),
-      "@modules": path.resolve(__dirname, "./src/modules"),
-
-    }
-  }
-})
+      '@': path.resolve(__dirname, './src'),
+      '@modules': path.resolve(__dirname, './src/modules'),
+      "@services":path.resolve(__dirname,'./src/services')
+    },
+  },
+});
